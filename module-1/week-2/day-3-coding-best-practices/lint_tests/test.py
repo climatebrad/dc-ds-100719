a1c0720e37ec182100562bd36d2db8e570a21b45
Brad Johnson <climatebrad@gmail.com>
2019-10-16 17:12:08 -0400

    passed pylint

--- module-1/week-2/day-3-coding-best-practices/lint_tests/test.py
+++ module-1/week-2/day-3-coding-best-practices/lint_tests/test.py
@@ -1,2 +1,7 @@
-def dB(x):
-    return x * 2
+"""This is a sample code file for a lesson in linting.
+It is the one of three modules.
+This one is the simplest of the three."""
+
+def multiply_by_two(val):
+    """returns input multiplied by two"""
+    return val * 2
